9fc05181155b227f3994e4ff551d2e8db59782e2
Nanakdeep <Nsidhu@thesummitgrp.com>
2024-02-06 13:42:37 +0530

    refactored token classification with seperate methods

--- services/issueTagger.py
+++ services/issueTagger.py
@@ -1,8 +1,7 @@
-from utils.io import FileProcessor
+
 from utils.fetchJiraIssues import get_issues
 from utils.tokenClassification import get_leaks
-from utils.constants import FileEntry
-from docx import Document
+
 
 def issueTagger():
     issues=get_issues("KAN")
--- utils/tokenClassification.py
+++ utils/tokenClassification.py
@@ -7,6 +7,7 @@ model = AutoModelForTokenClassification.from_pretrained(model_path)
 ## seperate out the the methods for processing comments description and title add file uploader create a class
 
 
+
 def get_leaks(issues):
 
     gen = pipeline("token-classification",model=model,tokenizer=tokenizer, device=-1)
@@ -14,42 +15,13 @@ def get_leaks(issues):
     ''' look for issue["fields"]["summary"], issue["fields"]["description"]["content"]["content"]["text"] , issue["fields"]["comment"]["comments"]["body"]["content"]["content"]["text"]
     '''   
     for issue in issues:
-        summary=issue["fields"]["summary"]
-        # print(issue)
-        PII_summary,PII_comments,PII_description=None,None,None
-        description=issue["fields"].get('description')
-        if description:
-            description=description["content"]
-            des_string=""
-            for i in description:
-                for j in i["content"]:
-                    # print(j)
-                    des_string+=j["text"]
-            description=des_string
-        # print("******************************************")
-        if description:
-            PII_description=gen(description, aggregation_strategy="first")
-        comments=issue["fields"]["comment"]["comments"]
-        # print(comments)
-        
         issue_review={}
-        PII_summary=gen(summary, aggregation_strategy="first")
+        PII_summary=get_summary_PII(issue,gen)
+        PII_description=get_description_PII(issue,gen)
+        PII_comments=get_comments_PII(issue,gen)
+        PII_attachments=get_attachment_PII(issue,gen)
 
-        PII_comments=[]
-        for comment in comments:
-            # print(comment["body"])
-            comment_data=comment["body"]["content"]
-            comment_string=""
-            for j in comment_data:
-                # print(j["content"])
-                for k in j["content"]:
-                    # print(k)
-                    comment_string+=k['text']
-            PII_comment=gen(comment_string, aggregation_strategy="first")
-            if PII_comment:
-                PII_comments.append({"id":f"{issue['id']}/{comment['id']}",'PII_leak':True,"Leaks":PII_comment})
-
-        if PII_summary or PII_comments or PII_description:
+        if PII_summary or PII_comments or PII_description or PII_attachments:
             issue_review['id']=issue["id"]
             issue_review['piiLeak']= True
             if PII_summary:
@@ -58,6 +30,8 @@ def get_leaks(issues):
                 issue_review["description"]=PII_description
             if PII_comments:
                 issue_review["comments"]=PII_comments
+            if PII_attachments:
+                issue_review['attachments']=PII_attachments
 
 
         else:
@@ -68,4 +42,44 @@ def get_leaks(issues):
         
     return tagged_issues
 
+def get_summary_PII(issue,gen):
+    summary=issue["fields"]["summary"]
+    return gen(summary, aggregation_strategy="first")
 
+def get_description_PII(issue,gen):
+    description=issue["fields"].get('description')
+    if description:
+            description=description["content"]
+            des_string=""
+            for i in description:
+                for j in i["content"]:
+                    # print(j)
+                    des_string+=j["text"]
+            description=des_string
+    if description:
+        return gen(description, aggregation_strategy="first")
+    return None
+
+def get_comments_PII(issue,gen):
+    comments=issue["fields"]["comment"]["comments"]
+    PII_comments=[]
+    for comment in comments:
+        comment_data=comment["body"]["content"]
+        comment_string=""
+        for j in comment_data:
+            for k in j["content"]:
+                comment_string+=k['text']
+        PII_comment=gen(comment_string, aggregation_strategy="first")
+        if PII_comment:
+            PII_comments.append({"id":f"{issue['id']}/{comment['id']}",'PII_leak':True,"Leaks":PII_comment})
+    return PII_comments
+
+def get_attachment_PII(issue,gen):
+    attachments= issue["fields"].get('attachment_data',[])
+    PII_attachments=[]
+    for attachment in attachments:
+        attachment_data=attachment["content"]
+        PII_attachment=gen(attachment_data, aggregation_strategy="first")
+        if PII_attachment:
+            PII_attachments.append({"filename":f"{attachment['filename']}",'PII_leak':True,"Leaks":PII_attachment})
+    return PII_attachments
